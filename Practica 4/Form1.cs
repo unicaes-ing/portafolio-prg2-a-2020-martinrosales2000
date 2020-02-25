d762644ce4e06c00de6437564302a73974ebe7e5
martinrosales2000 <martin.rosales@catolica.edu.sv>
2020-02-25 09:58:39 -0600

    practicas completas

---
+++ Practica 4/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static int sum(int n1=0, int n2=0, int n3=0, int n4=0)
+        {
+            return n1 + n2 + n3 + n4;
+        }
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtNum1.Text, out int num1) && int.TryParse(txtNum2.Text, out int num2) && int.TryParse(txtNum3.Text, out int num3) && int.TryParse(txtNum4.Text, out int num4))
+            {
+                lblSuma.Text = (sum(num1, num2, num3, num4)).ToString();
+                txtNum1.Clear();
+                txtNum2.Clear();
+                txtNum3.Clear();
+                txtNum4.Clear();
+            }
+            else
+            {
+                MessageBox.Show("ingrese numeros validos...");
+
+            }
+        }
+    }
+}
---
+++ Practica 4/Form2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_4
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtAgregar.Text, out int numero) || numero<=0)
+            {
+                lstNumeros.Items.Add(numero);
+                txtAgregar.Clear();
+                txtAgregar.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Agregue numeros validos...");
+            }
+        }
+        public static int buscar(int buscarnum, ListBox lista)
+        {
+            int contador = 0;
+            foreach (int n in lista.Items)
+            {
+                if (n == buscarnum) contador++;
+                
+            }
+            return contador;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numero = Convert.ToInt32(txtNumBuscar.Text);
+                int cant = buscar(numero, lstNumeros);
+                MessageBox.Show("el numero " + txtNumBuscar.Text + " se encuentra "  + cant + " veces ");
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("ingrese in numero a buscar...");
+                txtNumBuscar.Clear();
+                txtNumBuscar.Focus();
+            }
+        }
+    }
+}
